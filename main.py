a7b98e5c8b40ce6ea66dc850b048ba815d26104b
ruslanzakharov <ruslanzakharov3008@gmail.com>
2019-03-13 11:29:15 +0300

    Тест 1

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+from db import DB, NewsModel
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+api = Api(app)
+
+db = DB()
+
+
+def abort_if_news_not_found(news_id):
+    if not NewsModel(db.get_connection()).get(news_id):
+        abort(404, message='News {} not found'.format(news_id))
+
+
+class News:
+    def get(self, news_id):
+        abort_if_news_not_found(news_id)
+        news = NewsModel(db.get_connection()).get(news_id)
+
+
+
+class NewsList:
+    pass
+
 
 if __name__ == '__main__':
     app.run()
