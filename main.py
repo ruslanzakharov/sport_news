61e62e3eafa85d5483b9bfe372af26485b177cac
ruslanzakharov <ruslanzakharov3008@gmail.com>
2019-03-14 16:33:01 +0300

    Добавлены страницы регистрации, авторизации, успешности выполнения действия, а также главная

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dlfsgnre342jo4p1nler43p1n'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', [Length(min=4, max=25)])
+    password = PasswordField('Пароль', [Length(min=6, max=35)])
+    submit = SubmitField('Зарегистрироваться')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Главная')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html', title='Успех')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=8082)
